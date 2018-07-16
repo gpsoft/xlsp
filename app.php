fcbbf1dc7f6138d059f35c3eb4343a0887827244
gpsoft <etoh.gpsoft@gmail.com>
2018-07-16 14:08:36 +0900

    Enable to write in PDF

--- app.php
+++ app.php
@@ -9,10 +9,18 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 
+// hide grid lines
+$sheet->setShowGridLines(false);
+
 // set value
 $sheet->setCellValue('A1', 'Setting a simple value');
+$sheet->setCellValue('B1', 'This is B1');
+$sheet->setCellValue('A2', 'A2');
+$sheet->setCellValue('B2', 'B2');
 
-// save to file
+// save to files
 $writer = new Xlsx($spreadsheet);
 $writer->save('hello world.xlsx');
+$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Dompdf');
+$writer->save('hello world.pdf');
 
